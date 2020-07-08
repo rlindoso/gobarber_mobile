0f50baa8508641ee7765ab64ab8d66a199485336
Rafael Lindoso <rafael.lindoso@gmail.com>
2020-07-07 22:00:50 -0300

    Configurando Root Import

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   env: {
+    browser: true,
     es2020: true,
+    jest: true,
   },
   extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
   globals: {
@@ -16,15 +18,28 @@ module.exports = {
     ecmaVersion: 11,
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'prettier', 'jsx-a11y', 'import', 'react-hooks'],
   rules: {
     'prettier/prettier': 'error',
-    'react/jsx-filename-extension': [
-      'warn',
-      {
-        extensions: ['.jsx', '.js'],
-      },
-    ],
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
+
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'global-required': 'off',
+    'react-native/no-raw-text': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
   },
 };
